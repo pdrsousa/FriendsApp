6d7e4431b63c9b2dfdf653b46358f7f50062909b
pdrsousa <pdrmcsousa@gmail.com>
2020-12-27 00:08:45 +0000

    Changed about us page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,6 @@ class HomeController < ApplicationController
   end
 
   def about
+    @about_me = 'My name is Pedro and I\'m a software developer. This is an experimental app that manages friends.'
   end
 end
